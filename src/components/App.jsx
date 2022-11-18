00708253a1e516c4665a92f8705f20808d231575
tincurar <tincurares13@gmail.com>
2022-11-18 14:47:47 +0100

    added create and delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import defaultNotes from "../notes";
 import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function createNote(note) {
+        setNotes(previousNotes => {
+            return [...previousNotes, note];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(previousNotes => {
+            return previousNotes.filter((note, index) => {
+                return index !== id;
+            })
+        })
+    }
+
     return (<div>
-        <Header />
-        <CreateArea />
-        <Note
-            title="This is a new title"
-            content="This is a new paragraph"
-        />
-        {notes.map((note) =>
-            <Note
-                key={note.key}
-                title={note.title}
-                content={note.content}
-            />
-        )}
+        <div className="content">
+            <Header />
+            <CreateArea onAdd={createNote} />
+            {defaultNotes.map((note) =>
+                <Note
+                    key={note.key}
+                    id={note.key}
+                    title={note.title}
+                    content={note.content}
+                    onDelete={deleteNote}
+                />
+            )}
+            {notes.map((note, index) =>
+                <Note
+                    key={index}
+                    id={index}
+                    title={note.title}
+                    content={note.content}
+                    onDelete={deleteNote}
+                />
+            )}
+        </div>
         <Footer />
     </div>);
 }
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function changedNote(event) {
+    const { name, value } = event.target;
+    setNote(previousNote => {
+      return {
+        ...previousNote,
+        [name]: value
+      }
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={changedNote} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={changedNote} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
